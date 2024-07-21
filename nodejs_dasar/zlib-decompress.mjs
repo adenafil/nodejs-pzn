b262a3f43d3276ffec11715faf463c66111215a4
adenafil <nafilie9@gmail.com>
2024-07-22 00:45:11 +0700

    tamat

--- nodejs_dasar/zlib-decompress.mjs
+++ nodejs_dasar/zlib-decompress.mjs
@@ -4,4 +4,4 @@ import fs from 'fs';
 const source = fs.readFileSync('zlib-compress.mjs.txt');
 const result = zlib.unzipSync(source);
 
-console.info(result.toString());
+console.info(result.toString()) 
