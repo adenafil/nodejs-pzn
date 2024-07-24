3ac7c299efba6de7aa71f7b1ab05f2b8ac8fdf35
adenafil <nafilie9@gmail.com>
2024-07-25 02:12:05 +0700

    76. Jest Command Line Interface

--- belajar-nodejs-unit-test/test/sum.test.js
+++ belajar-nodejs-unit-test/test/sum.test.js
@@ -1,6 +1,20 @@
 import {sum} from "../src/sum.js";
 
-test("test some function", () => {
+test("test some function 1", () => {
+
+    const result = sum(2, 3);
+
+    expect(result).toEqual(5);
+})
+
+test("test some function 2", () => {
+
+    const result = sum(2, 3);
+
+    expect(result).toEqual(5);
+})
+
+test("test some function 3", () => {
 
     const result = sum(2, 3);
 
