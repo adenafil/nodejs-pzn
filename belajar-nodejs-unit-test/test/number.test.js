cfc7cefb42c9054c1154ba053c7d65abab4430eb
adenafil <nafilie9@gmail.com>
2024-07-25 20:11:01 +0700

    80. Numbers Matchers

---
+++ belajar-nodejs-unit-test/test/number.test.js
@@ -0,0 +1,10 @@
+test("number", () => {
+    const value = 2 + 2;
+    expect(value).toBeGreaterThan(3);
+    expect(value).toBeGreaterThanOrEqual(3.5);
+    expect(value).toBeLessThan(5);
+    expect(value).toBeLessThanOrEqual(4.5);
+
+    expect(value).toBe(4);
+    expect(value).toEqual(4);
+});
