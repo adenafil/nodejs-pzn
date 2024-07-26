9bcb1f39a8c3aad8e822cb57e64d59ee6a731375
adenafil <nafilie9@gmail.com>
2024-07-26 22:58:16 +0700

    95. Mock Function

--- belajar-nodejs-unit-test/src/sum.js
+++ belajar-nodejs-unit-test/src/sum.js
@@ -9,3 +9,19 @@ export const sumAll = (numbers) => {
     }
     return total;
 }
+
+export const calculate = (numbers, callback) => {
+    let total = 0;
+    for (numbers of numbers) {
+        total += numbers;
+    }
+    callback(total);
+}
+
+export const calculateAndReturn = (numbers, callback) => {
+    let total = 0;
+    for (numbers of numbers) {
+        total += numbers;
+    }
+    return callback(total);
+}
