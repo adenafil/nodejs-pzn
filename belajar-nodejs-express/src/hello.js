f484a3c309ef752ef053facfd4ea73cf682834d1
adenafil <nafilie9@gmail.com>
2024-07-31 03:38:30 +0700

    127. Basic Routing

--- belajar-nodejs-express/src/hello.js
+++ belajar-nodejs-express/src/hello.js
@@ -2,6 +2,15 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.get('/ade', (req, res) => {
+    res.send('Hello ade!');
+})
+
+
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
 });
