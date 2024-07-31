e476e51778f921c156129616ed22841f1e18edca
adenafil <nafilie9@gmail.com>
2024-08-01 00:12:11 +0700

    133. Response

---
+++ belajar-nodejs-express/test/response.test.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import request from 'supertest';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello response');
+})
+
+test("Test Response", async () => {
+    const response = await request(app).get('/');
+    expect(response.text).toBe('Hello response');
+    expect(response.status).toBe(200);
+});
