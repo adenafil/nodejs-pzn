12f8d01a3582e58ede19b86c2aa3b6184e7dcd43
adenafil <nafilie9@gmail.com>
2024-08-03 02:24:03 +0700

    151. File Upload

--- belajar-nodejs-express/test/response.test.js
+++ belajar-nodejs-express/test/response.test.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import request from 'supertest';
+import expressFileUpload from 'express-fileupload';
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(expressFileUpload());
 
 app.post('/json', (req, res) => {
     const name = req.body.name;
@@ -18,6 +20,23 @@ app.post('/form', (req, res) => {
         hello: `Hello ${name}`,
     })
 })
+
+app.post('/file', async (req, res) => {
+    const textFile = req.files.article;
+    await textFile.mv(__dirname + "/upload/" + textFile.name)
+
+    res.send(`Hello ${req.body.name}, you uploaded ${textFile.name}`);
+});
+
+test("Test Request File Upload", async () => {
+    const response = await request(app).post('/file')
+        .set({'content-type': 'multipart/form-data'})
+        .field("name", "ade")
+        .attach("article", __dirname + "/contoh.txt")
+    ;
+    expect(response.text).toBe('Hello ade, you uploaded contoh.txt');
+});
+
 
 test("Test Request JSON", async () => {
     const response = await request(app).post('/json')
