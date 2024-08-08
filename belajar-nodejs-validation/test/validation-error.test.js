9d1171203a90efd475272db6324431d63acc33ce
adenafil <nafilie9@gmail.com>
2024-08-08 23:15:49 +0700

    213. Validation Error

---
+++ belajar-nodejs-validation/test/validation-error.test.js
@@ -0,0 +1,16 @@
+import Joi from "joi";
+
+describe("Joi", () => {
+    it("should return validation error", () => {
+        const usernameSchema = Joi.string().min(5).email().required();
+
+        const result = usernameSchema.validate("ups");
+        console.info(result);
+
+        if (result.error) {
+            result.error.details.forEach(detail => {
+                console.info(`${detail.path} = ${detail.message}`);
+            })
+        }
+    })
+})
