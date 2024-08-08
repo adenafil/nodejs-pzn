214675c1c3489ece9685451dd2fb61e600d48678
adenafil <nafilie9@gmail.com>
2024-08-08 20:40:13 +0700

    210. Validation

---
+++ belajar-nodejs-validation/test/validation.test.js
@@ -0,0 +1,28 @@
+import Joi from "joi";
+
+describe("Joi", () => {
+    it("should can create schema", function() {
+        const schema = Joi.string().min(3).max(100).required();
+
+        const result = schema.validate("ade");
+        if (result.error) {
+            console.log(result.error);
+        }
+    })
+
+    it("should can validate basic data type", function() {
+        const usernameSchema = Joi.string().email().required();
+        const isAdminSchema = Joi.boolean().required();
+        const priceSchema = Joi.number().required().min(1000).max(1_000_000);
+
+        const resultUsername = usernameSchema.validate("ade@gmail.com");
+        console.info(resultUsername);
+
+        const resultIsAdmin = isAdminSchema.validate("false");
+        console.info(resultIsAdmin);
+
+        const resultPrice = priceSchema.validate(10_000);
+        console.info(resultPrice);
+
+    })
+})
