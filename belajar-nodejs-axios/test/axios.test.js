f9ef137f5cb30375abc8070e090b1295819a661e
adenafil <nafilie9@gmail.com>
2024-08-18 03:54:32 +0700

    287. HTTP Method

--- belajar-nodejs-axios/test/axios.test.js
+++ belajar-nodejs-axios/test/axios.test.js
@@ -10,3 +10,15 @@ describe("HTTP Client", () => {
 
     })
 })
+
+describe("HTTP Method", () => {
+    const httpClient = axios.create({
+        baseURL: "https://enajdec1qsqs7.x.pipedream.net",
+        timeout: 5000,
+    })
+
+    it("support GET Method", async () => {
+        const response = await httpClient.get("/");
+        expect(response.status).toBe(200);
+    })
+})
